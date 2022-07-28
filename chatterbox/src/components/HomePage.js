088d662c4beb622af59807836615e9b2df03585f
Sourabh4471 <moriarty4471@gmail.com>
2022-07-28 15:27:15 +0530

    sourabh

--- chatterbox/src/components/HomePage.js
+++ chatterbox/src/components/HomePage.js
@@ -1,24 +1,41 @@
-import React, { useEffect } from "react";
-import {Link, useNavigate} from "react-router-dom";
-import styles from "./styles/HomePage.module.css";
+import { Link } from "react-router-dom";
+import background1 from "./assets/white.jpeg";
+import background2 from "./assets/white.jpeg";
+import cric from "./assets/olalogo.png";
+import { useEffect, useState } from "react";
 
-//bootstrap components
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+const HomePage = () => {
 
-export default function HomePage() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (localStorage.getItem("Token") !== null) {
-      navigate("/newsfeed");
-    }
-  });
-  return (
-    //using bootstrap and custom css to make homepage
-    {}
+    let [user, setUser] = useState({});
+
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('loggedInUser')));
+        console.log(JSON.parse(localStorage.getItem('loggedInUser')));
+    }, []);
+
+  
+
+    return (
+        <div>
+            
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                            <ul class="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link text-white" to="/register" >REGISTER</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link text-white" to="/login" >LOGIN</Link>
+                                </li>
+                            </ul>
+                        </div>
+
+
+        </div>
+
+    );
     
-  );
+   
+
 }
- 
+
+export default HomePage;
